1f7eb2b534056f9b107e85eb4954670c4ec927aa
FlorianIF <f_marcelo@hotmail.fr>
2017-04-28 14:25:14 +0200

    Update custom.js

--- bitcoin/custom.js
+++ bitcoin/custom.js
@@ -1,54 +1,18 @@
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-if (this.readyState == 4 && this.status == 200) {
-var myObj = this.responseText;
-var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-document.getElementById("demo").innerHTML = jsonPretty;
-}
-};
-xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
-xmlhttp.send();
+$(document).ready(function() {
+	$.ajax({
+		url : "https://api.blockcypher.com/v1/btc/main",
+		dataType : "json",
+		contentType : "application/json; charset=utf-8",
+		type : "GET",
+		timeout:	"5000",
+		async : false,
 
-var xmlhttp1 = new XMLHttpRequest();
-xmlhttp1.onreadystatechange = function() {
-if (this.readyState == 4 && this.status == 200) {
-var myObj = this.responseText;
-var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-document.getElementById("demo1").innerHTML = jsonPretty;
-}
-};
-xmlhttp1.open("GET", "http://bitcoin.mubiz.com/info", true);
-xmlhttp1.send();
+		success : function(data) {
+			$('#bitcoin_block_number').append(data.height);
+		},
 
-var xmlhttp2 = new XMLHttpRequest();
-xmlhttp2.onreadystatechange = function() {
-if (this.readyState == 4 && this.status == 200) {
-var myObj = this.responseText;
-var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-document.getElementById("demo2").innerHTML = jsonPretty;
-}
-};
-xmlhttp2.open("GET", "http://bitcoin.mubiz.com/blockchaininfo", true);
-xmlhttp2.send();
-
-var xmlhttp3 = new XMLHttpRequest();
-xmlhttp3.onreadystatechange = function() {
-if (this.readyState == 4 && this.status == 200) {
-var myObj = this.responseText;
-var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-document.getElementById("demo3").innerHTML = jsonPretty;
-}
-};
-xmlhttp3.open("GET", "http://bitcoin.mubiz.com/mininginfo", true);
-xmlhttp3.send();
-
-var xmlhttp4 = new XMLHttpRequest();
-xmlhttp4.onreadystatechange = function() {
-if (this.readyState == 4 && this.status == 200) {
-var myObj = this.responseText;
-var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-document.getElementById("demo4").innerHTML = jsonPretty;
-}
-};
-xmlhttp4.open("GET", "http://bitcoin.mubiz.com/peerinfo", true);
-xmlhttp4.send();
+		error : function(xhr, status, err) {
+			$('#bitcoin_block_number').append(err+" N/A");
+		}
+	});
+});
